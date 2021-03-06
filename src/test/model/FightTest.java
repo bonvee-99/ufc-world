8a295ff091632720afc0116b64cb144f974f958c
Ben Vinnick <benvinnick@gmail,com>
2021-03-06 13:16:11 -0800

    fixed assertions again

--- src/test/model/FightTest.java
+++ src/test/model/FightTest.java
@@ -3,8 +3,6 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 // Tests for the Fight class
--- src/test/model/UfcWorldTest.java
+++ src/test/model/UfcWorldTest.java
@@ -1,10 +1,9 @@
 package model;
 
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UfcWorldTest {
     UfcWorld myWorld;
